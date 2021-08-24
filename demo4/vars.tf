5363f44bb3626115a2412e25f71bbe118baced39
Shreehari <shreehari.kodange@gmail.com>
2021-08-24 15:38:28 +0530

    Added Nginx Provisioner

---
+++ demo4/vars.tf
@@ -0,0 +1,30 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+
+variable "AWS_SECRET_KEY" {
+
+}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-0279406e0655775be"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
